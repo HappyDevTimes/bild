ff02c4e4cf98ea9e0848ea71b7f060212206cca5
HappyDevTimes <34409045+HappyDevTimes@users.noreply.github.com>
2022-02-26 23:34:08 +0100

    Update PixaBay.js

--- scripts/PixaBay.js
+++ scripts/PixaBay.js
@@ -4,79 +4,109 @@ var searchQ = "";
 var colorQ = "";
 var URL = "";
 
-$("#sokbutton").click(() => {
-    search()
+document.getElementById('sokbutton').addEventListener("click", async function () {
+    fetchImages()
 });
 
-$(document).ready(() => {
-    search();
-});
+(function () {
+    checkButtonState();
+    fetchImages();
+})();
 
-function search() {
-    searchQ = $("#searchfield").val();
-    colorQ = $('#colors').find(":selected").text().toLowerCase();
-    URL = "https://pixabay.com/api/?key=" + API_KEY + "&q=" + encodeURIComponent(searchQ) + "&colors=" + encodeURIComponent(colorQ) + "&per_page=10&page=" + pageNumber;
-    $('#image-container').empty();
-    $.getJSON(URL, function (data) {
-        console.log(data)
-        if (parseInt(data.totalHits) > 0)
-
-            $.each(data.hits, function (i, hit) {
-                console.log(hit.previewURL);
-                // $('#image-container').append("<div id='imagegrid'><img src=" + hit.largeImageURL + "></img></div>");
-
-                var grid = document.createElement('div');
-                grid.setAttribute("id", "imagegrid");
-                grid.innerHTML = "<img src=" + hit.largeImageURL + "></img>";
-
-                var user = document.createElement("p");
-                user.innerHTML = hit.user
-
-                var tags = document.createElement("p");
-                tags.innerHTML = hit.tags
-
-                grid.append(user);
-                grid.append(tags);
-                $('#image-container').append(grid);
-
-
-            });
-        else
-            console.log('No hits');
-    });
+function checkButtonState() {
+    if (pageNumber == 1) {
+        document.getElementById('previousButton').disabled = true;
+    } else {
+        document.getElementById('previousButton').disabled = false;
+    }
 }
 
-$('#nextButton').click(function () {
-    pageNumber++;
-    $('#image-container').empty();
+async function fetchImages() {
+    searchQ = document.getElementById('searchfield').value;
+    var sel = document.getElementById("colors");
+    colorQ = sel.options[sel.selectedIndex].text.toLowerCase();
+    document.getElementById('image-container').innerHTML = "";
     URL = "https://pixabay.com/api/?key=" + API_KEY + "&q=" + encodeURIComponent(searchQ) + "&colors=" + encodeURIComponent(colorQ) + "&per_page=10&page=" + pageNumber;
-    $.getJSON(URL, function (data) {
-        if (parseInt(data.totalHits) > 0)
+    const data = await (await fetch(URL)).json();
 
-            $.each(data.hits, function (i, hit) {
-                console.log(hit.previewURL);
-                $('#image-container').append("<div id='imagegrid'><img src=" + hit.largeImageURL + "></img></div>");
-            });
-        else
-            console.log('No hits');
-    });
+    if (parseInt(data.totalHits) > 0)
+        data.hits.forEach(hit => {
+            console.log(hit.previewURL);
+            var grid = document.createElement('div');
+            grid.setAttribute("id", "imagegrid");
+            grid.innerHTML = "<img src=" + hit.largeImageURL + "></img>";
+
+            var user = document.createElement("p");
+            user.innerHTML = hit.user
+
+            var tags = document.createElement("p");
+            tags.innerHTML = hit.tags
+
+            grid.append(user);
+            grid.append(tags);
+            document.getElementById('image-container').append(grid);
+        });
+
+    else
+        console.log('No hits');
+}
+
+document.getElementById('nextButton').addEventListener("click", async function () {
+    pageNumber++;
+    checkButtonState();
+    document.getElementById('image-container').innerHTML = "";
+    URL = "https://pixabay.com/api/?key=" + API_KEY + "&q=" + encodeURIComponent(searchQ) + "&colors=" + encodeURIComponent(colorQ) + "&per_page=10&page=" + pageNumber;
+    var data = await (await fetch(URL)).json();
+
+    if (parseInt(data.totalHits) > 0)
+        data.hits.forEach(hit => {
+            console.log(hit.previewURL);
+            var grid = document.createElement('div');
+            grid.setAttribute("id", "imagegrid");
+            grid.innerHTML = "<img src=" + hit.largeImageURL + "></img>";
+
+            var user = document.createElement("p");
+            user.innerHTML = hit.user
+
+            var tags = document.createElement("p");
+            tags.innerHTML = hit.tags
+
+            grid.append(user);
+            grid.append(tags);
+            document.getElementById('image-container').append(grid);
+        });
+
+    else
+        console.log('No hits');
+
 });
 
-$(function () {
-    $('#previousButton').click(function () {
-        if (pageNumber == 1) return;
-        pageNumber--;
-        URL = "https://pixabay.com/api/?key=" + API_KEY + "&q=" + encodeURIComponent(searchQ) + "&colors=" + encodeURIComponent(colorQ) + "&per_page=10&page=" + pageNumber;
-        $('#image-container').empty();
-        $.getJSON(URL, function (data) {
-            if (parseInt(data.totalHits) > 0)
+document.getElementById('previousButton').addEventListener("click", async function () {
+    if (pageNumber == 1) return;
+    pageNumber--;
+    checkButtonState();
+    document.getElementById('image-container').innerHTML = "";
+    URL = "https://pixabay.com/api/?key=" + API_KEY + "&q=" + encodeURIComponent(searchQ) + "&colors=" + encodeURIComponent(colorQ) + "&per_page=10&page=" + pageNumber;
+    var data = await (await fetch(URL)).json();
 
-                $.each(data.hits, function (i, hit) {
-                    console.log(hit.previewURL);
-                    $('#image-container').append("<div id='imagegrid'><img src=" + hit.largeImageURL + "></img></div>");
-                });
-            else
-                console.log('No hits');
+    if (parseInt(data.totalHits) > 0)
+        data.hits.forEach(hit => {
+            console.log(hit.previewURL);
+            var grid = document.createElement('div');
+            grid.setAttribute("id", "imagegrid");
+            grid.innerHTML = "<img src=" + hit.largeImageURL + "></img>";
+
+            var user = document.createElement("p");
+            user.innerHTML = hit.user
+
+            var tags = document.createElement("p");
+            tags.innerHTML = hit.tags
+
+            grid.append(user);
+            grid.append(tags);
+            document.getElementById('image-container').append(grid);
         });
-    });
+
+    else
+        console.log('No hits');
 });
